7df37b83ec41e81e3d24bace142ba38932b1c27c
marcelobarud <77701362+marcelobarud@users.noreply.github.com>
2026-09-10 13:15:42 -0300

    fix: refina visualização das fotos da galeria

--- apps/web/src/goal-gallery.test.tsx
+++ apps/web/src/goal-gallery.test.tsx
@@ -19,6 +19,7 @@ describe('GoalGallery', () => {
     expect(view.getByAltText('Primeiro marco').getAttribute('crossorigin')).toBe('use-credentials');
     fireEvent.click(view.getByRole('button', { name: /Primeiro marco/ }));
     expect(view.getByRole('dialog')).toBeTruthy();
+    expect(view.getByRole('heading', { name: 'Primeiro marco' })).toBeTruthy();
     expect(view.getByText('Um avanço importante.')).toBeTruthy();
     expect(view.getAllByAltText('Primeiro marco')[1].getAttribute('crossorigin')).toBe('use-credentials');
   });
